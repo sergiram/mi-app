776e1f58945b62731dd80748d9f89ec40d77cd62
Sergi <sergiram94@gmail.com>
2023-09-22 11:56:14 +0200

    images and css

--- src/Nav.js
+++ src/Nav.js
@@ -1,1 +1,16 @@
-return <nav></nav>
+return (
+
+    <nav>
+        <img src="littlelemonlogo.jpg" />
+        <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#menu">Menu</a></li>
+            <li><a href="#reservations">Reservations</a></li>
+            <li><a href="#order">Order Online</a></li>
+            <li><a href="#login">Login</a></li>
+        </ul>
+    </nav>
+
+
+)
