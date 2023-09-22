b42850f79703b0f8838a2da83c74ce74636f9ac3
Sergi <sergiram94@gmail.com>
2023-09-22 10:51:51 +0200

    meta tags & components

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+return (
+
+    <header>
+        <meta name="description" content="your text goes here"/>
+        <meta name="og:title" content=""/>
+        <meta name="og:description" content=""/>
+        <meta name="og:image" content=""/>
+    </header>
+
+
+)
